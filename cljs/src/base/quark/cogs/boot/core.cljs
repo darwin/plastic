3fd2cb9c0891d7e836eef3e495ff0d0c329c3052
Antonin Hildebrand <antonin@hildebrand.cz>
2015-06-27 19:11:59 +0200

    rename boot.handlers to boot.init

--- cljs/src/base/quark/cogs/boot/core.cljs
+++ cljs/src/base/quark/cogs/boot/core.cljs
@@ -1,3 +1,3 @@
 (ns quark.cogs.boot.core
-  (:require [quark.cogs.boot.handlers])
+  (:require [quark.cogs.boot.init])
   (:require-macros [quark.macros.logging :refer [log info warn error group group-end]]))
--- cljs/src/base/quark/cogs/boot/init.cljs
+++ cljs/src/base/quark/cogs/boot/init.cljs
@@ -1,4 +1,4 @@
-(ns quark.cogs.boot.handlers
+(ns quark.cogs.boot.init
   (:require [quark.frame.core :refer [register-handler]]
             [quark.schema.paths :as paths])
   (:require-macros [quark.macros.logging :refer [log info warn error group group-end]]))
