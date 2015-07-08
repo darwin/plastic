adec102e733b75c2f91281819d3ecb666f341da7
Antonin Hildebrand <antonin@hildebrand.cz>
2015-07-08 09:47:07 +0200

    bump dependencies

--- cljs/project.clj
+++ cljs/project.clj
@@ -2,17 +2,17 @@
   :description "Quark - Experimental ClojureScript editor component for Atom"
   :url "http://github.com/darwin/quark"
 
-  :dependencies [[org.clojure/clojure "1.7.0-beta3"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "0.0-3422"]
                  [org.clojure/tools.reader "0.10.0-SNAPSHOT"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [rewrite-cljs "0.2.0"]
+                 [rewrite-cljs "0.3.0"]
                  [binaryage/devtools "0.1.2"]               ; Electron 0.28.2 has old Blink, we have to stick with this old version of devtools for now
-                 [figwheel "0.3.3"]
+                 [figwheel "0.3.7"]
                  [rm-hull/inkspot "0.0.1-SNAPSHOT"]
                  [spellhouse/phalanges "0.1.6"]
                  [reagent "0.5.0" :exclusions [cljsjs/react]]
-                 [cljsjs/react "0.12.2-5"]]
+                 [cljsjs/react "0.13.3-0"]]
 
   :plugins [[lein-cljsbuild "1.0.5"]
             [lein-figwheel "0.3.3"]]
