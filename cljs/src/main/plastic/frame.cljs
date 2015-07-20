6fb392612b1fcf61bea343bbbc79bf1de9d6c32f
Antonin Hildebrand <antonin@hildebrand.cz>
2015-07-20 15:26:50 +0200

    own router-loop, don't give browser control between dispatches

--- cljs/src/main/plastic/frame.cljs
+++ cljs/src/main/plastic/frame.cljs
@@ -1,8 +1,11 @@
 (ns plastic.frame
-  (:require-macros [plastic.macros.logging :refer [log info warn error group group-end measure-time]])
+  (:require-macros [plastic.macros.logging :refer [log info warn error group group-end measure-time]]
+                   [cljs.core.async.macros :refer [go-loop go]])
   (:require [plastic.frame.core :as frame]
             [plastic.frame.router :as router]
-            [clojure.string :as string]))
+            [clojure.string :as string]
+            [plastic.frame.handlers :refer [handle]]
+            [cljs.core.async :refer [chan put! <!]]))
 
 (defn simple-printer [acc val]
   (conj acc (cond
@@ -27,4 +30,26 @@
    (frame/register-handler id [timing frame/trim-v middleware] handler)))
 
 (def subscribe frame/subscribe)
-(def router-loop router/router-loop)
+
+(defn router-loop []
+  (go-loop []
+    (let [event-v (<! router/event-chan)]                   ;; wait for an event
+      (try
+        (handle event-v)
+
+        ;; If the handler throws:
+        ;;   - allow the exception to bubble up because the app, in production,
+        ;;     may have hooked window.onerror and perform special processing.
+        ;;   - But an exception which bubbles up will break the enclosing go-loop.
+        ;;     So we'll need to start another one.
+        ;;   - purge any pending events, because they are probably related to the
+        ;;     event which just fell in a screaming heap. Not sane to handle further
+        ;;     events if the prior event failed.
+        (catch :default e
+          (do
+            ;; try to recover from this (probably uncaught) error as best we can
+            (router/purge-chan)                             ;; get rid of any pending events
+            (router-loop)                                   ;; Exception throw will cause termination of go-loop. So, start another.
+
+            (throw e)))))                                   ;; re-throw so the rest of the app's infrastructure (window.onerror?) gets told
+    (recur)))
