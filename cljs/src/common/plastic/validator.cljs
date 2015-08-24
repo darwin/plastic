e9cd7fb030531511bb5d1813734a84a6d71f6052
Antonin Hildebrand <antonin@hildebrand.cz>
2015-08-24 23:23:47 +0200

    implement general schema validation caching
    in general, a single data object can be validated against multiple schemas
    
    https://github.com/Prismatic/schema/issues/256

--- cljs/src/common/plastic/validator.cljs
+++ cljs/src/common/plastic/validator.cljs
@@ -5,17 +5,34 @@
 
 ; shared validation utils
 
-; see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/WeakSet
-(assert js/WeakSet)
-(defonce *valid-objects* (js/WeakSet.))
+(assert js/WeakSet)                                                                                                   ; see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/WeakSet
+(assert js/WeakMap)                                                                                                   ; see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/WeakMap
+(defonce *cached-validations* (js/WeakMap.))                                                                          ; object -> WeakSet of schemas
+
+(defn cache-hit? [obj schema]
+  (if-let [schema-set (.get *cached-validations* obj)]
+    (.has schema-set schema)))
+
+(defn make-weak-set [& items]
+  (let [weak-set (js/WeakSet.)]
+    (doseq [item items]
+      (.add weak-set item))
+    weak-set))
+
+(defn cache-validation [obj schema]
+  (if-let [schema-set (.get *cached-validations* obj)]
+    (.add schema-set schema)
+    (.set *cached-validations* obj (make-weak-set schema))))
 
 (defn check-or-update-cache! [schema obj]
-  (if (.has *valid-objects* obj)
-    true
-    (when (nil? (s/check schema obj))
-      (.add *valid-objects* obj)
-      true)))
+  (if (goog/isObject obj)                                                                                             ; WeakMap must have object keys, anyways primitive values are cheap to check
+    (if (cache-hit? obj schema)
+      true
+      (when (nil? (s/check schema obj))                                                                               ; when validation fails (rare), bail out and let else branch of s/if do proper validation reporting
+        (cache-validation obj schema)
+        true))))
 
-;  to speed up validation we can cache some expensive validation results
+; to speed up validation we can cache some expensive validation results
+; see https://github.com/Prismatic/schema/issues/256
 (defn cached [schema]
   (s/if (partial check-or-update-cache! schema) s/Any schema))
