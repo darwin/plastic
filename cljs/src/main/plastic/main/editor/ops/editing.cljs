e66191973c636679087d4cc2f9558e1e9fd775ca
Antonin Hildebrand <antonin@hildebrand.cz>
2015-08-30 15:30:35 +0200

    split editing.cljs into helpers.cljs and editing.cljs

--- cljs/src/main/plastic/main/editor/ops/editing.cljs
+++ cljs/src/main/plastic/main/editor/ops/editing.cljs
@@ -1,67 +1,13 @@
 (ns plastic.main.editor.ops.editing
-  (:require-macros [plastic.logging :refer [log info warn error group group-end]]
-                   [plastic.main :refer [worker-dispatch worker-dispatch-with-effect worker-dispatch-args]])
-  (:require [plastic.main.frame :refer [subscribe register-handler]]
-            [plastic.main.editor.model :as editor]
+  (:require-macros [plastic.logging :refer [log info warn error group group-end]])
+  (:require [plastic.main.editor.model :as editor]
             [plastic.main.editor.ops.cursor :as cursor]
-            [plastic.main.editor.toolkit.id :as id]))
-
-(defn xform-event [editor & args]
-  (vec (concat [:editor-xform (editor/get-id editor)] args)))
-
-(defn xform-editor-on-worker [editor args & [callback]]
-  (worker-dispatch-args (apply xform-event editor args)
-    (if callback (editor/db-updater (editor/get-id editor) callback)))
-  editor)
-
-(defn continue [cb]
-  (if cb cb identity))
-
-(defn move-cursor-for-case-of-selected-node-removal [editor]
-  (let [cursor-id (id/id-part (editor/get-cursor editor))
-        moves-to-try (if (= cursor-id (editor/get-focused-form-id editor))
-                       [:move-next-form :move-prev-form]                                                              ; a case of deleting whole focused form
-                       [:structural-left :structural-right :structural-up])]
-    (apply cursor/apply-move-cursor editor moves-to-try)))
-
-(defn editing-string? [editor]
-  (and (editor/editing? editor) (= (editor/get-inline-editor-mode editor) :string)))
-
-(defn get-edit-point [editor]
-  (if (editor/editing? editor)
-    (editor/get-editing editor)
-    (editor/get-cursor editor)))
-
-(defn walk-structural-web [web start path]
-  (let [walker (fn [pos dir]
-                 (let [info (get web pos)
-                       _ (assert info)
-                       new-pos (dir info)]
-                   new-pos))]
-    (reduce walker start path)))
-
-(defn should-commit? [editor]
-  (or (editor/is-inline-editor-empty? editor) (editor/is-inline-editor-modified? editor)))                            ; empty inline editor is a placeholder and must be comitted regardless
-
-(defn select-neighbour [form-id edit-point path editor]
-  (let [structural-web (editor/get-structural-web-for-form editor form-id)
-        target-node-id (walk-structural-web structural-web edit-point path)]
-    (editor/set-cursor editor target-node-id)))
-
-(defn enable-editing-mode [editor]
-  (-> editor
-    (editor/set-editing (editor/get-cursor editor))
-    (editor/set-inline-editor-puppets-state true)))                                                                   ; puppets should get enabled for each new editing session
-
-(defn disable-editing-mode [editor]
-  (-> editor
-    (editor/set-editing nil)))
-
-(defn sanitize-cursor [editor alt-cursor]
-  (-> editor
-    (editor/replace-cursor-if-not-valid alt-cursor)))
-
-; -------------------------------------------------------------------------------------------------------------------
+            [plastic.main.editor.toolkit.id :as id]
+            [plastic.main.editor.ops.editing.helpers :refer [xform-editor-on-worker continue editing-string?
+                                                             move-cursor-for-case-of-selected-node-removal
+                                                             get-edit-point walk-structural-web select-neighbour
+                                                             should-commit? sanitize-cursor disable-editing-mode
+                                                             enable-editing-mode]]))
 
 (declare perform-space)
 
