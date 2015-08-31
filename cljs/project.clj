63962bb9f714f8b3776e34899df78249b07aebf9
Antonin Hildebrand <antonin@hildebrand.cz>
2015-08-31 10:58:07 +0200

    bump clojurescript to 1.7.122

--- cljs/project.clj
+++ cljs/project.clj
@@ -4,7 +4,7 @@
 
   :dependencies
   [[org.clojure/clojure "1.7.0"]
-   [org.clojure/clojurescript "1.7.107"]
+   [org.clojure/clojurescript "1.7.122"]
    [org.clojure/tools.reader "0.10.0-SNAPSHOT"]
    [org.clojure/core.async "0.1.346.0-17112a-alpha"]
    [com.cognitect/transit-cljs "0.8.225"]
