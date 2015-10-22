darwin/plastic

commit bd4c21a99fddcd8dafd887db679863512be197ff

monkey patching is bad for you
update our dirty patches to track
https://github.com/atom/atom/pull/8811

lib/plastic-editor-view.coffee
@@ -9,24 +9,15 @@ lastId = 0
 monkeyPatchPresenterInstance = (editorView) ->
   presenter = editorView.component.presenter
   throw "presenter on editorView.component not found" unless presenter
-  originalMethod = presenter.updateContentDimensions
-  throw "presenter.updateContentDimensions not found" unless originalMethod
-  presenter.updateContentDimensions = ->
-    if @lineHeight?
-      oldContentHeight = @contentHeight
-      @contentHeight = @lineHeight * @model.getScreenLineCount()
-
+  originalMethod = presenter.updateHorizontalDimensions
+  throw "presenter.updateHorizontalDimensions not found" unless originalMethod
+  updateHorizontalDimensions: ->
     if @baseCharacterWidth?
       oldContentWidth = @contentWidth
       clip = @model.tokenizedLineForScreenRow(@model.getLongestScreenRow())?.isSoftWrapped()
       @contentWidth = @pixelPositionForScreenPosition([@model.getLongestScreenRow(), @model.getMaxScreenLineLength()], clip).left
       @contentWidth += @scrollLeft
       @contentWidth += 1 unless @model.isSoftWrapped() # account for cursor width
-
-    if @contentHeight isnt oldContentHeight
-      @updateHeight()
-      @updateScrollbarDimensions()
-      @updateScrollHeight()
 
     if @contentWidth isnt oldContentWidth
       # <<<<<<<<<< next line seamlesly expands editor content frame as user is typing

commit d0168b3ae93244ba20fedf4a13647f2a2101b913

fix broken monkey patching business
regression from bd4c21a99fddcd8dafd887db679863512be197ff

lib/plastic-editor-view.coffee
@@ -11,7 +11,7 @@ monkeyPatchPresenterInstance = (editorView) ->
   throw "presenter on editorView.component not found" unless presenter
   originalMethod = presenter.updateHorizontalDimensions
   throw "presenter.updateHorizontalDimensions not found" unless originalMethod
-  updateHorizontalDimensions: ->
+  presenter.updateHorizontalDimensions = ->
     if @baseCharacterWidth?
       oldContentWidth = @contentWidth
       clip = @model.tokenizedLineForScreenRow(@model.getLongestScreenRow())?.isSoftWrapped()