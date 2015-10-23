72e503b4e8bf0db94db2f956c42cb2077f1f4d5f
Antonin Hildebrand <antonin@hildebrand.cz>
2015-10-23 16:25:16 +0200

    REPL echoing to devtools console works again
    and the implementation is much less hacky

--- src/dev/plastic/dev/figwheel.cljs
+++ src/dev/plastic/dev/figwheel.cljs
@@ -6,10 +6,8 @@
             [figwheel.client.socket :as socket]
             [figwheel.client.utils :as utils]))
 
-(defn eval [code]
-  (if plastic.env.need-loophole
-    (.runInThisContext (js/require "vm") code)                                                                        ; https://github.com/atom/loophole
-    (js* "eval(~{code})")))
+(defonce ^:dynamic *inside-repl-plugin* false)
+(def ^:const repl-style "color:white; background-color:black; padding:0px 2px; border-radius:2px;")
 
 (defn figwheel-repl-fix [code]
   (.replace code
@@ -17,51 +15,41 @@
     "(function (){try{return cljs.core.identity.call"))
 
 (defn intellij-repl-fix [code]
-  (.replace code #"^try\{cljs\.core\.pr_str\.call" "try{cljs.core.identity.call"))
+  (.replace code
+    #"^try\{cljs\.core\.pr_str\.call"
+    "try{cljs.core.identity.call"))
 
-; I wasn't able to remove pr-str wrapper on figwheel side, this is a dirty hack
-(defn patch-code [code]
+(defn rewrite-repl-code-snippet [code]
   (-> code figwheel-repl-fix intellij-repl-fix))
 
-(defn eval-javascript** [code result-handler]
-  (try
-    (binding [*print-fn* (fn [& args] (-> args figwheel/console-print figwheel/figwheel-repl-print))
-              *print-newline* false]
-      (let [res (eval code)]
-        (if res
-          (.log js/console "F>" res))
-        (result-handler {:status :success
-                         :value  (pr-str res)})))
-    (catch js/Error e
-      (result-handler {:status     :exception
-                       :value      (pr-str e)
-                       :stacktrace (string/join "\n" (figwheel/truncate-stack-trace (.-stack e)))
-                       :base-path  (utils/base-url-path)}))
-    (catch :default e
-      (result-handler {:status     :exception
-                       :value      (pr-str e)
-                       :stacktrace "No stacktrace available."}))))
+(defn eval [code]
+  (if plastic.env.need-loophole
+    (.runInThisContext (js/require "vm") code)                                                                        ; https://github.com/atom/loophole
+    (js* "eval(~{code})")))
 
-(defn repl-plugin [_]
-  (fn [[{:keys [msg-name] :as msg} & _]]
-    (when (= :repl-eval msg-name)
-      (figwheel/ensure-cljs-user)
-      (eval-javascript** (patch-code (:code msg))
-        (fn [res]
-          (socket/send! {:figwheel-event "callback"
-                         :callback-name  (:callback-name msg)
-                         :content        res}))))))
+(defn present-repl-result [result]
+  (log "%cREPL" repl-style result))
 
-(defn on-js-load []
-  ;(.remount-editors js/plastic.onion.remounter)
-  ;(dispatch :editor-update-layout)
-  )
+(defn fancy-eval [code]
+  (if-not *inside-repl-plugin*
+    (eval code)
+    (let [rewritten-code (rewrite-repl-code-snippet code)
+          result (eval rewritten-code)]
+      (present-repl-result result)
+      (pr-str result))))
+
+(defn repl-plugin [& args]
+  (let [standard-impl (apply figwheel/repl-plugin args)]
+    (fn [& args]
+      (binding [*inside-repl-plugin* true]
+        (apply standard-impl args)))))
+
+(defn on-js-load [])
 
 (when-not plastic.env.dont-start-figwheel
   (figwheel/start
     {:on-jsload     on-js-load
-     :eval-fn       eval
+     :eval-fn       fancy-eval
      :websocket-url "ws://localhost:7000/figwheel-ws"
      :build-id      'dev
-     ; :merge-plugins {:repl-plugin repl-plugin}
-     }))
+     :merge-plugins {:repl-plugin repl-plugin}}))
